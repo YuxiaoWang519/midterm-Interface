9fcbe66e490a671baf8c84814455814f7dd447e9
YuxiaoWang519 <84213964+YuxiaoWang519@users.noreply.github.com>
2022-10-18 21:03:27 -0400

    2

--- interfacemidterm/src/index.js
+++ interfacemidterm/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/midterm-Interface/">
   
     <React.StrictMode>
       <App />
